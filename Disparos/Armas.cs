1b2260664de75e4fd1ea04c6baa475ca671e0017
Darxelf <darwin.jimenez12@gmail.com>
2023-07-06 23:38:07 -0400

    Add project files.

---
+++ Disparos/Armas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disparos.Disparos
+{
+   public  class Armas
+    {
+        int power { get; set; }
+        float range { get; set;}
+        int speed { get; set; } 
+        public virtual void disparar() 
+        {
+            Console.WriteLine($"El disparo tiene un poder de:{power} y un rango de:{range}");
+        }
+    }
+    public class Pistola : Armas 
+    {
+        public override void disparar()
+        {
+            Console.WriteLine("");
+        }
+    }
+}
